c00194a9cf2da5eeb9238c1db66762bc5fe8e516
kaikaic1998 <kainianc@gmail.com>
2023-04-13 21:28:08 +0900

    Add Closure implementation with lambda expression

--- Dynamic_Programming/closure/main.cpp
+++ Dynamic_Programming/closure/main.cpp
@@ -2,14 +2,29 @@
 
 #include <iostream>
 #include <functional>
-#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
-void memoizedAddTo80 (){
-    vector<int> cache = {};
+function<int (int)> memoizedAddTo80 (){ // create a function object
+    unordered_map<int, int> cache; // create map to store key and its value, in this case [8] = 85
+    return [cache](int n) mutable{ // return lambda expression, must mutable because changing cahce map
+        if (cache.count(n)){
+            return cache[n];
+        }
+        else{
+            cout << "long time" << endl;
+            cache[n] = n + 80;
+            return cache[n];
+        }
+    };
 }
 
 int main(){
+    function<int (int)> memoized = memoizedAddTo80();
 
+    cout << "1 " << memoized(5) << endl;
+    cout << "2 " << memoized(5) << endl;
+
+    return 0;
 }
